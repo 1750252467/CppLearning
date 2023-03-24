4a203916278b329a76c59f873bc84ffa6612f720
1750252467 <1750252467@qq.com>
2023-03-24 09:18:25 +0800

    fyk

---
+++ LeetCode/459.重复的子字符串.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=459 lang=cpp
+ * @lcpr version=21801
+ *
+ * [459] 重复的子字符串
+ */
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lc code=start
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+
+    }
+};
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// "abab"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aba"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abcabcabcabc"\n
+// @lcpr case=end
+
+ */
+
+
